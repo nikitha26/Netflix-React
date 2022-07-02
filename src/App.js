88c29543620e414f7c564f7b1b37b3b53b2ef095
nikitha26 <mnikitha365@gmail.com>
2022-07-02 21:43:56 +0530

    bug

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Row from "./Row";
+import Banner from './Banner';
+import Navbar from './Navbar';
+import requests from './requests';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Banner />
+      <Row title="Netflix Originals" 
+         fetchUrl={requests.fetchTrending}
+         isLargeRow = {true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchNetflixOriginal}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,36 @@
+import React, {useState,useEffect } from 'react'
+import './nav.css';
+
+function Navbar() {
+  const [show, handleShow] = useState(false);
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        if(window.scrollY > 100){
+            handleShow(true);
+        }else{
+            handleShow(false);
+        }
+      });
+    
+      return () => {
+        window.removeEventListener("scroll",null);
+      };
+    }, []);
+    
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+       <img
+        className='nav_logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
+        alt='nav_logo'
+       />
+       <img
+        className='nav_avatar'
+        src='http://pbs.twimg.com/profile_images/2284174872/7df3h38zabcvjylnyfe3_normal.png'
+        alt='avatar_logo'
+       />
+    </div>
+  )
+}
+
+export default Navbar
